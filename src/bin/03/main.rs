2312f3f250372491a94236065333db185d97b324
Friedrich Schwedler <f.schwedler@stud.uni-goettingen.de>
2021-12-06 01:18:09 +0100

    day 3 part 2

--- src/bin/03/main.rs
+++ src/bin/03/main.rs
@@ -23,6 +23,14 @@ impl BinaryDiagnostic {
             sum: [0; BITS],
         }
     }
+    fn from_bits(bits: &[u8; BITS]) -> BinaryDiagnostic {
+        let mut bd = BinaryDiagnostic::new();
+        bd.num = 1;
+        for i in 0..BITS {
+            bd.sum[i] = bits[i] as usize;
+        }
+        bd
+    }
     fn rate<F>(&self, compare: F) -> u16
         where F: Fn(usize, usize) -> bool {
         self.sum.into_iter().fold(0, |acc, i| {
@@ -34,12 +42,10 @@ impl BinaryDiagnostic {
         })
     }
     fn gamma_rate(&self) -> u16 {
-        //self.rate( |i, num| 2*i > num)
-        self.rate( |i, num| i > num/2)
+        self.rate( |i, num| 2*i >= num)
     }
     fn epsilon_rate(&self) -> u16 {
-        //self.rate( |i, num| 2*i < num)
-        self.rate( |i, num| i < num/2)
+        self.rate( |i, num| 2*i < num)
     }
     fn result(&self) -> u32 {
         self.gamma_rate() as u32 * self.epsilon_rate() as u32
@@ -81,16 +87,82 @@ impl Add<&BinaryDiagnostic> for BinaryDiagnostic {
     }
 }
 
+fn bits_from_str(s: &str) -> Result<[u8; BITS], SimpleError> {
+    let inpt = s.as_bytes();
+    if inpt.len() != BITS {
+        return Err(SimpleError::new(format!("{:?} has the wrong length", s)));
+    }
+    let mut arr = [0; BITS];
+    for i in 0..BITS {
+        arr[i] = match inpt[i] {
+            b'1' => 1,
+            b'0' => 0,
+            e => return Err(SimpleError::new(format!("{:?} is not 1|0", e)))
+        };
+    }
+    Ok(arr)
+}
+
+fn oxygen_co2_step(arr: &Vec<[u8; BITS]>, i: usize, cmp: fn(usize, usize) -> bool) -> Result<Vec<[u8; BITS]>, SimpleError> {
+    if i >= BITS {
+        return Err(SimpleError::new(format!("there are less then {} bits", i)));
+    }
+    let (ones, zeros) = arr.iter().fold((0, 0), |(ones, zeros), v| {
+        if 1 == v[i] {
+            (ones + 1, zeros)
+        } else {
+            (ones, zeros + 1)
+        }
+    });
+    let mut tmp = Vec::new();
+    for entry in arr.iter().filter(|v| {
+        (v[i] == 1) == cmp(ones, zeros)
+    }) {
+        tmp.push(*entry);
+    }
+    Ok(tmp)
+}
+
+fn build_number(number: &[u8; BITS]) -> u32 {
+    number.iter().fold(0, |num, x| num * 2 + *x as u32)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let foo = ReaderBuilder::new()
+    let (foo, arr) = ReaderBuilder::new()
         .has_headers(false)
         .from_reader(include_str!("input.txt").as_bytes())
         .records()
         .into_iter()
-        .fold(Ok(BinaryDiagnostic::new()), |acc: Result<BinaryDiagnostic, Box<dyn Error>>, i| {
-            Ok(acc? + &BinaryDiagnostic::from_str(i?.as_slice())?)
+        .fold(Ok((BinaryDiagnostic::new(), Vec::new())), |acc: Result<(BinaryDiagnostic, Vec<[u8; BITS]>), Box<dyn Error>>, i| {
+            let (acu, mut arr) = acc?;
+
+            let line = bits_from_str(&i?.as_slice())?;
+            let bd = BinaryDiagnostic::from_bits(&line);
+            arr.push(line);
+
+            Ok((acu + &bd, arr))
         })?;
     println!("γ: {}, ε: {}, γ × ε = {}", foo.gamma_rate(), foo.epsilon_rate(), foo.result());
+
+    let mut oxygen = arr.clone();
+    for o in 0..BITS {
+        oxygen = oxygen_co2_step(&oxygen, o, |ones, zeros| ones >= zeros)?;
+        if oxygen.len() == 1 {
+            break;
+        }
+    }
+
+    let mut co2 = arr;
+    for o in 0..BITS {
+        co2 = oxygen_co2_step(&co2, o, |ones, zeros| ones < zeros)?;
+        if co2.len() == 1 {
+            break;
+        }
+    }
+    let oxygen = build_number(&oxygen[0]);
+    let co2 = build_number(&co2[0]);
+    println!("oxygen: {}, co2: {}, oxygen × co2 = {}", oxygen, co2, oxygen*co2);
+
 
     Ok(())
 }
