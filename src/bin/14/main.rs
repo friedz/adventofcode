676b4bdfc0ab7c5b66d0ab0fc0c0f042d008a027
Friedrich Schwedler <f.schwedler@stud.uni-goettingen.de>
2021-12-14 20:25:50 +0100

    day 14 part 2

--- src/bin/14/main.rs
+++ src/bin/14/main.rs
@@ -1,21 +1,22 @@
 
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    hash::Hash,
+    ops::AddAssign,
+};
 use simple_error::{
-    SimpleError,
     simple_error,
     SimpleResult,
 };
 
-fn max_and_min(polymer: &str) -> SimpleResult<((char, usize), (char, usize))> {
-    let count_map = polymer.chars().fold(HashMap::new(), |mut counts, element| {
-        if let Some(count) = counts.get_mut(&element) {
-            *count += 1;
-        } else {
-            counts.insert(element, 1);
-        }
-        counts
+type PolyMap = HashMap<(char, char), usize>;
+type LookUp = HashMap<(char, char), char>;
+
+fn max_and_min(poly_map: &PolyMap) -> SimpleResult<((char, usize), (char, usize))> {
+    let count_map = poly_map.iter().fold(HashMap::new(), |mut count_map, ((_, k), v)| {
+        insert_or_add(&mut count_map, k, v);
+        count_map
     });
-    println!("{:?}", count_map);
     let max = count_map.iter().max_by(|a, b| a.1.cmp(&b.1))
         .ok_or(simple_error!("No maximum found?!?"))?;
     let min = count_map.iter().min_by(|a, b| a.1.cmp(&b.1))
@@ -24,13 +25,44 @@ fn max_and_min(polymer: &str) -> SimpleResult<((char, usize), (char, usize))> {
     Ok(((*max.0, *max.1), (*min.0, *min.1)))
 }
 
+fn genererate_pair_map(polymer: &str) -> PolyMap {
+    polymer.chars().fold((None, HashMap::new()), |(last, mut polymer_map), c| {
+        if let Some(last) = last {
+            if let Some(val) = polymer_map.get_mut(&(last, c)) {
+                *val += 1;
+            } else {
+                polymer_map.insert((last, c), 1);
+            }
+        }
+        (Some(c), polymer_map)
+    }).1
+}
+fn insert_or_add<K: Eq + Hash + Copy, V: AddAssign + Copy>(map: &mut HashMap<K, V>, key: &K, val: &V) {
+    if let Some(v) = map.get_mut(key) {
+        *v += *val;
+    } else {
+        map.insert(*key, *val);
+    }
+}
+fn transform_map(polymer_map: PolyMap, lookup: &LookUp) -> PolyMap {
+    polymer_map.into_iter().fold(PolyMap::new(), |mut poly_map, ((k_left, k_right), v)| {
+        if let Some(insert) = lookup.get(&(k_left, k_right)) {
+            insert_or_add(&mut poly_map, &(k_left, *insert), &v);
+            insert_or_add(&mut poly_map, &(*insert, k_right), &v);
+        } else {
+            insert_or_add(&mut poly_map, &(k_left, k_right), &v);
+        }
+        poly_map
+    })
+}
+
 fn main() -> SimpleResult<()> {
     //let input = include_str!("example_input.txt");
     let input = include_str!("input.txt");
     let mut input = input.split("\n\n");
     let polymer = input.next().ok_or(simple_error!("No polymer providet!"))?;
     let lookup = input.next().ok_or(simple_error!("No pair insetion rules providet!"))?
-        .lines().try_fold(HashMap::new(), |mut lookup, line| -> SimpleResult<HashMap<(char, char), char>> {
+        .lines().try_fold(LookUp::new(), |mut lookup, line| -> SimpleResult<LookUp> {
             let mut elm = line.split(" -> ");
 
             let mut key = elm.next().ok_or(simple_error!("No pair to transform providet!"))?.trim().chars();
@@ -43,32 +75,18 @@ fn main() -> SimpleResult<()> {
             lookup.insert((a, b), val);
             Ok(lookup)
         })?;
-    //println!("{}", polymer);
+    println!("{}", polymer);
     //println!("{:?}", lookup);
 
-    let mut polymer = polymer.to_string();
-    //let mut polymer_1 = String::new();
+    let mut poly_map = genererate_pair_map(polymer);
+    //println!("{:?}", poly_map);
     for i in 1..=40 {
-        polymer = polymer.chars().fold((None, String::new()), |(last, mut polymer), c| {
-            if let Some(last) = last {
-                if let Some(insert) = lookup.get(&(last, c)) {
-                    polymer.push(*insert);
-                }
-            }
-            polymer.push(c);
-            (Some(c), polymer)
-        }).1;
-        //println!("{}: {}", i, polymer);
-        if 10 == i {
-            let (max, min) = max_and_min(&polymer)?;
-            //polymer_1 = polymer.clone();
+        poly_map = transform_map(poly_map, &lookup);
+        if 10 == i || 40 == i {
+            let (max, min) = max_and_min(&poly_map)?;
             println!("Max: {:?}, Min: {:?} -> {}", max, min, max.1 - min.1);
         }
     }
-    //let (max, min) = max_and_min(&polymer_1)?;
-    //println!("Max: {:?}, Min: {:?} -> {}", max, min, max.1 - min.1);
-    let (max, min) = max_and_min(&polymer)?;
-    println!("Max: {:?}, Min: {:?} -> {}", max, min, max.1 - min.1);
 
     Ok(())
 }
