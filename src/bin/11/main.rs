eeb402d0e005ac3511488a29f4a68d0eacff592d
Friedrich Schwedler <f.schwedler@stud.uni-goettingen.de>
2022-12-12 06:17:05 +0100

    day 11 part 2

--- src/bin/11/main.rs
+++ src/bin/11/main.rs
@@ -23,27 +23,39 @@ use nom::{
 };
 
 struct Monkey {
-    items: VecDeque<i32>,
-    operation: Box<dyn Fn(i32) -> i32>,
-    inspection_count: u32,
-    test: i32,
+    items: VecDeque<i64>,
+    operation: Box<dyn Fn(i64) -> i64>,
+    inspection_count: u64,
+    test: i64,
     pass_to: (usize, usize),
+    safe_multiple: Option<i64>,
 }
 impl Monkey {
     #[cfg(test)]
-    fn new(items: Vec<i32>, op: Box<dyn Fn(i32) -> i32>, test: i32, pass: (usize, usize)) -> Monkey {
+    fn new(items: Vec<i64>, op: Box<dyn Fn(i64) -> i64>, test: i64, pass: (usize, usize)) -> Monkey {
         Monkey {
             items: VecDeque::from(items),
             operation: op,
             inspection_count: 0,
             test: test,
             pass_to: pass,
+            safe_multiple: None,
         }
     }
-    fn turn(&mut self) -> Option<(usize, i32)> {
+    fn turn(&mut self, managing: bool) -> Option<(usize, i64)> {
         match self.items.pop_front() {
             Some(worry) => {
-                let worry = (self.operation)(worry) / 3;
+                let worry = (self.operation)(worry);
+                let worry = if managing {
+                    worry / 3
+                } else {
+                    worry
+                };
+                let worry = match self.safe_multiple {
+                    Some(n) => worry % n,
+                    None => worry,
+                };
+                self.inspection_count += 1;
                 if 0 == worry % self.test {
                     Some((self.pass_to.0, worry))
                 } else {
@@ -53,18 +65,38 @@ impl Monkey {
             None => None,
         }
     }
-    fn add_item(&mut self, item: i32) {
+    fn add_item(&mut self, item: i64) {
         self.items.push_back(item);
+    }
+    fn safe_multiple(&mut self, safe: i64) {
+        self.safe_multiple = Some(safe);
+    }
+}
+fn primfactors(n: i64) -> Vec<i64> {
+    let mut n = n;
+    let mut res = Vec::new();
+    let mut i = 2;
+    loop {
+        if n % i == 0 {
+            n = n / i;
+            res.push(i);
+            if n == 1 {
+                return res;
+            }
+        } else {
+            i += 1;
+        }
     }
 }
 impl Debug for Monkey {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Point")
             .field("items", &self.items)
-            .field("operation", &"|u32| -> i32")
+            .field("operation", &"|u64| -> i64")
             .field("inspection_count", &self.inspection_count)
             .field("test", &self.test)
             .field("pass_to", &self.pass_to)
+            .field("safe_multiple", &self.safe_multiple)
             .finish()
     }
 }
@@ -73,9 +105,10 @@ impl Default for Monkey {
         Monkey {
             items: VecDeque::default(),
             operation: Box::new(move |old| old),
-            inspection_count: u32::default(),
-            test: i32::default(),
+            inspection_count: u64::default(),
+            test: i64::default(),
             pass_to: (usize::default(), usize::default()),
+            safe_multiple: None,
         }
     }
 }
@@ -85,6 +118,7 @@ impl PartialEq for Monkey {
             && self.inspection_count == other.inspection_count
             && self.test == other.test
             && self.pass_to == other.pass_to
+            && self.safe_multiple == other.safe_multiple
     }
 }
 fn monkey_number(s: &str) -> IResult<&str, usize> {
@@ -95,12 +129,12 @@ fn monkey_number(s: &str) -> IResult<&str, usize> {
     let (s, _) = tag(":")(s)?;
     Ok((s, i as usize))
 }
-fn starting_items(s: &str) -> IResult<&str, VecDeque<i32>> {
+fn starting_items(s: &str) -> IResult<&str, VecDeque<i64>> {
     //   Starting items: 79, 98
     let (s, _) = space0(s)?;
     let (s, _) = tag("Starting items:")(s)?;
     let (s, _) = space0(s)?;
-    let (s, v) = separated_list0(tag(", "), complete::i32)(s)?;
+    let (s, v) = separated_list0(tag(", "), complete::i64)(s)?;
     Ok((s, VecDeque::from(v)))
 }
 #[derive(Debug, Copy, Clone)]
@@ -111,9 +145,9 @@ enum Op {
 #[derive(Debug, Copy, Clone)]
 enum Operand {
     Old,
-    Num(i32),
+    Num(i64),
 }
-fn operation(s: &str) -> IResult<&str, Box<dyn Fn(i32) -> i32>> {
+fn operation(s: &str) -> IResult<&str, Box<dyn Fn(i64) -> i64>> {
     //   Operation: new = old * 19
     let (s, _) = space0(s)?;
     let (s, _) = tag("Operation: new = old")(s)?;
@@ -125,7 +159,7 @@ fn operation(s: &str) -> IResult<&str, Box<dyn Fn(i32) -> i32>> {
     let (s, _) = space0(s)?;
     let (s, right) = alt((
             combinator::value(Operand::Old, tag("old")),
-            combinator::map(complete::i32, |i| Operand::Num(i)),
+            combinator::map(complete::i64, |i| Operand::Num(i)),
     ))(s)?;
     Ok((s, match (op, right) {
         (Op::Add, Operand::Old) => Box::new(move |old| old + old),
@@ -134,12 +168,12 @@ fn operation(s: &str) -> IResult<&str, Box<dyn Fn(i32) -> i32>> {
         (Op::Mult, Operand::Num(i)) => Box::new(move |old| old * i),
     }))
 }
-fn test(s: &str) -> IResult<&str, i32> {
+fn test(s: &str) -> IResult<&str, i64> {
     //   Test: divisible by 23
     let (s, _) = space0(s)?;
     let (s, _) = tag("Test: divisible by")(s)?;
     let (s, _) = space0(s)?;
-    let (s, i) = complete::i32(s)?;
+    let (s, i) = complete::i64(s)?;
     Ok((s, i))
 }
 fn pass_to(s: &str) -> IResult<&str, (usize, usize)> {
@@ -175,22 +209,68 @@ fn monkey(s: &str) -> IResult<&str, (usize, Monkey)> {
     Ok((s, (idx, Monkey {
         items: items,
         operation: op,
-        inspection_count: u32::default(),
+        inspection_count: u64::default(),
         test: test,
         pass_to: pass,
+        safe_multiple: None,
     })))
 }
 fn monkey_list(s: &str) -> IResult<&str, Vec<Monkey>> {
      let (s, v) = separated_list0(pair(newline, newline), monkey)(s)?;
      Ok((s, v.into_iter().map(|(_, monk)| monk).collect()))
 }
+fn merge_factors(a: &Vec<i64>, b: &Vec<i64>) -> Vec<i64> {
+    let mut res = Vec::new();
+    let (mut ai, mut bi) = (0, 0);
+    while ai < a.len() && bi < b.len() {
+        if a[ai] == b[bi] {
+            res.push(a[ai]);
+            ai += 1;
+            bi += 1;
+        } else if a[ai] < b[bi] {
+            res.push(a[ai]);
+            ai += 1;
+        } else {
+            res.push(b[bi]);
+            bi += 1;
+        }
+    }
+    if ai < a.len() {
+        res.extend_from_slice(&a[ai..]);
+    }
+    if bi < b.len() {
+        res.extend_from_slice(&b[bi..]);
+    }
+    res
+}
+fn find_safe_multiple(ml: &Vec<Monkey>) -> i64 {
+    let mut factors = Vec::new();
+    for m in ml.iter().map(|m| primfactors(m.test)) {
+        factors = merge_factors(&factors, &m);
+    }
+    factors.iter().fold(1, |res, a| res * a)
+}
 
-fn part1(monkeys: &mut Vec<Monkey>) -> u32 {
+fn part1(monkeys: &mut Vec<Monkey>) -> u64 {
     for _ in 0..20 {
         for i in 0..monkeys.len() {
-            while let Some((to, item)) = monkeys[i].turn() {
+            while let Some((to, item)) = monkeys[i].turn(true) {
                 monkeys[to].add_item(item);
-                monkeys[i].inspection_count += 1;
+            }
+        }
+    }
+    monkeys.sort_by(|a, b| b.inspection_count.cmp(&a.inspection_count));
+    monkeys[0].inspection_count * monkeys[1].inspection_count
+}
+fn part2(monkeys: &mut Vec<Monkey>) -> u64 {
+    let safe = find_safe_multiple(monkeys);
+    for m in monkeys.iter_mut() {
+        m.safe_multiple(safe);
+    }
+    for _ in 0..10_000 {
+        for i in 0..monkeys.len() {
+            while let Some((to, item)) = monkeys[i].turn(false) {
+                monkeys[to].add_item(item);
             }
         }
     }
@@ -198,10 +278,13 @@ fn part1(monkeys: &mut Vec<Monkey>) -> u32 {
     monkeys[0].inspection_count * monkeys[1].inspection_count
 }
 
+
 fn main() {
     let input = include_str!("input.txt");
     let (_, mut monkeys) = monkey_list(input).unwrap();
     println!("Part 1: {}", part1(&mut monkeys));
+    let (_, mut monkeys) = monkey_list(input).unwrap();
+    println!("Part 2: {}", part2(&mut monkeys));
 }
 
 #[cfg(test)]
@@ -250,6 +333,11 @@ mod tests_day_11 {
         assert_eq!(part1(&mut monkeys), 10605);
     }
     #[test]
+    fn part2_example() {
+        let (_, mut monkeys) = monkey_list(INPUT).unwrap();
+        assert_eq!(part2(&mut monkeys), 2713310158);
+    }
+    #[test]
     fn parse_all_monkeys() {
         let monkeys = parsed_monkeys();
         let (_, read_monkeys) = monkey_list(INPUT).unwrap();
@@ -258,13 +346,12 @@ mod tests_day_11 {
     #[test]
     fn parse_monkey() {
         let (left, (idx, monk)) = monkey(SINGLE_MONKEY).unwrap();
-        let correct_monkey = Monkey {
-            items: VecDeque::from([79, 98]),
-            operation: Box::new(move |worry| worry),
-            inspection_count: 0,
-            test: 23,
-            pass_to: (2, 3),
-        };
+        let correct_monkey = Monkey::new(
+            vec![79, 98],
+            Box::new(move |worry| worry),
+            23,
+            (2, 3),
+        );
         assert_eq!(idx, 0);
         assert_eq!(monk, correct_monkey);
         for i in 0..20 {
@@ -358,5 +445,11 @@ mod tests_day_11 {
         for i in 0..100 {
             assert_eq!(op(i), i + 3);
         }
+    }
+    #[test]
+    fn prims() {
+        assert_eq!(primfactors(12), [2, 2, 3]);
+        assert_eq!(primfactors(147), [3, 7, 7]);
+        assert_eq!(primfactors(84), [2, 2, 3, 7]);
     }
 }
